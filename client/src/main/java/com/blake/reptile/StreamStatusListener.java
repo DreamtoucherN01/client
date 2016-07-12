96086d9c349d0eec83c8f1eb9817884cd6238e9c
Jack <Jack@DESKTOP-GP6FBN7>
2016-07-12 10:22:54 +0800

    initial commit

---
+++ client/src/main/java/com/blake/reptile/StreamStatusListener.java
@@ -0,0 +1,62 @@
+package com.blake.reptile;
+
+import net.sf.json.JSONObject;
+
+import com.blake.util.Constants;
+import com.blake.util.HttpRequest;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.json.DataObjectFactory;
+
+@SuppressWarnings("deprecation")
+public class StreamStatusListener implements StatusListener{
+
+	public void onException(Exception ex) {
+		
+		ex.printStackTrace();
+	}
+
+	public void onStatus(Status status) {
+
+		String str = DataObjectFactory.getRawJSON(status);              
+        try {  
+        	
+            DBObject dbObject =(DBObject)JSON.parse(str);  
+            
+            JSONObject jo = new JSONObject();
+	        jo.put("type", "insert");
+	        jo.put("body", dbObject.toString());
+            
+            HttpRequest.sendPost(Constants.serverUrl, jo.toString());
+            System.out.println(dbObject.get("text"));  
+        }  catch (Exception e) {  
+            e.printStackTrace();  
+        }   
+	}
+
+	public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+		
+		System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId()); 
+	}
+
+	public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+
+		System.out.println("Got track limitation notice:" + numberOfLimitedStatuses); 
+	}
+
+	public void onScrubGeo(long userId, long upToStatusId) {
+
+		System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
+	}
+
+	public void onStallWarning(StallWarning warning) {
+
+		System.out.println("Got stall warning:" + warning);  
+	}
+
+}
---
+++ client/src/main/java/com/blake/util/Constants.java
@@ -0,0 +1,21 @@
+package com.blake.util;
+
+
+public class Constants {
+
+	public static String consumerKey = "bLx4Xr9gh4jjFHboL1KzSuvgU";
+	
+	public static String consumerSecret = "15DZzC8HJtfW0T523Lo7G16Ow0ONtDWxirHdY4GWEtG5udeRtl";
+	
+	public static String accessToken = "750254868279861248-tulPT6UW1DtZPwvOQsCx5ZoSz0W5UJY";
+	
+	public static String accessTokenKey = "ewgbVUBP9rWgoDGq2Sim55dtltwfyr96Dksg4g1SjGcdT";
+	
+	public static String oauthUrl = "https://api.twitter.com/oauth2/token";
+	
+	public static String twitterUrl = "https://api.twitter.com/1.1/direct_messages/sent.json"; 
+	
+	public static String twitterParams = "count=2&since_id=240247560269340670";
+	
+	public static String serverUrl = "http://127.0.0.1:8080/server/api/request"; 
+}
