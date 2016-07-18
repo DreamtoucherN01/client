98b41561d75a598fa2c6aabf79144cd418c0f633
Jack Cheung <Jack Cheung@Jack>
2016-07-18 14:30:49 +0800

    changes

--- client/src/main/java/com/blake/util/Constants.java
+++ client/src/main/java/com/blake/util/Constants.java
@@ -17,5 +17,4 @@ public class Constants {
 	
 	public static String twitterParams = "count=2&since_id=240247560269340670";
 	
-	public static String serverUrl = "http://202.114.107.225:8080/server/api/request"; 
 }
